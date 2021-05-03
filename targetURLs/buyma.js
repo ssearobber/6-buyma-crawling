c3e21ec1c61a1755e9d91bd795a571094c648534
ssearobber <ssearobber@naver.com>
2021-05-03 23:45:45 +0900

    buyma 크롤링

--- targetURLs/buyma.js
+++ targetURLs/buyma.js
@@ -27,7 +27,7 @@ async function buyma() {
         height: 1080,
     });
     await page.setDefaultNavigationTimeout(0);
-    await page.goto('https://www.buyma.com/my/sell/?tab=b');
+    await page.goto('https://www.buyma.com/my/sell?duty_kind=all&order=desc&page=1&rows=100&sale_kind=all&sort=item_id&status=for_sale&timesale_kind=all#/');
 
     // 로그인 작업 건너뛰기
     if (await page.$('.user_name')) {
@@ -42,13 +42,39 @@ async function buyma() {
         console.log('로그인했습니다.')
     }
 
+    // 데이터 크롤링
+    let result = [];
+    let today = new Date();
+    let year = today.getFullYear(); // 년도
+    let month = today.getMonth() + 1;  // 월
+    let date = today.getDate();  // 날짜
+    today = year + '/' + month + '/' + date;
+    result = await page.evaluate((today) => {
+        const tags = document.querySelectorAll('table tbody tr:nth-child(even)');
+        const result = [];
+        tags.forEach((t)=> {
+            result.push({
+                productName : t && t.querySelector('.item_name p:nth-of-type(1)') && t.querySelector('.item_name p:nth-of-type(1)').textContent,
+                productId : t && t.querySelector('.item_name p:nth-of-type(2) a:nth-of-type(1)') && t.querySelector('.item_name p:nth-of-type(2) a:nth-of-type(1)').textContent,
+                releaseDate : t && t.querySelector('._item_yukodate span') && t.querySelector('._item_yukodate span').textContent.replace(/\n\s+/g,''),
+                cart :  t && t.querySelector('td:nth-of-type(11) span') && t.querySelector('td:nth-of-type(11) span').textContent,
+                wish :  t && t.querySelector('td:nth-of-type(12) span') && t.querySelector('td:nth-of-type(12) span').textContent,
+                access :  t && t.querySelector('td:nth-of-type(13) span') && t.querySelector('td:nth-of-type(13) span').textContent,
+                today : today
+            })
+        });
+        return result;
+    }, today)
+
+    // db 등록
+    console.log("result" ,result);
+
     await page.close();
     await browser.close();
 
     }
     catch(e) {
         console.log(e);
-
         await page.close();
         await browser.close();
 
