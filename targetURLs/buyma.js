062dfe93ab6f83fb45f9261249368a8d35529e6c
ssearobber <ssearobber@naver.com>
2021-11-21 15:22:49 +0900

    make it better

--- targetURLs/buyma.js
+++ targetURLs/buyma.js
@@ -36,8 +36,11 @@ async function buyma() {
     //     height: 1080,
     // });
     await page.setDefaultNavigationTimeout(0);
-    await page.goto('https://www.buyma.com/my/sell?duty_kind=all&order=desc&page=1&rows=100&sale_kind=all&sort=item_id&status=for_sale&timesale_kind=all#/', {
+    const response = await page.goto('https://www.buyma.com/my/sell?duty_kind=all&order=desc&page=1&rows=100&sale_kind=all&sort=item_id&status=for_sale&timesale_kind=all#/', {
 	waitUntil: 'networkidle0'});
+    if (!response) {
+        throw "Failed to load page!";
+    }
 
     // 로그인 작업 건너뛰기
     if (await page.$('.user_name')) {
